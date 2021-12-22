77677c393e7aba184c9753dd39e31b920b56ea42
dd2676 <dd2676@columbia.edu>
2021-12-22 11:04:02 -0500

    3rd commit

--- src/predict_spam_function.py
+++ src/predict_spam_function.py
@@ -1,27 +1,32 @@
-#
-#  Copyright 2019-2020 Viens Consulting, LLC. All Rights Reserved.
-#
-import os
+import os, io, sys
 
 import boto3
-import json
+import json, csv
 import logging
 import sms_spam_classifier_utilities
+import re
 
+import email
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from botocore.exceptions import ClientError
 
+import numpy as np
+from sms_spam_classifier_utilities import one_hot_encode, vectorize_sequences
+import time
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Vocabulary Size for one_hot_encode, vectorize_sequences
+vocabulary_length = 9013
 # Character encoding for reply email
 REPLY_CHARSET = "utf-8"
 REPLY_TEMPLATE = {
-  "from_sender": "Deepak Dwarakanath<hw3@viegco.com>",
-  "to_recipient": "Steve Viens<steve@viens.net>",
-  "subject": "We received your email sent at [EMAIL_RECEIVE_DATE] with the subject [EMAIL_SUBJECT]",
-  "text_body": "Here is a 240 character sample of the email body: [EMAIL_BODY]\n\nThe email was categorized as [CLASSIFICATION] with a [CLASSIFICATION_CONFIDENCE_SCORE]% confidence.",
+  "from_sender": "Deepak EmailServer<dd2676@deepak-dwarakanath.com>",
+  "to_recipient": "Deepak Dwarakanath<dd2676@columbia.edu>",
+  "subject": "We received your email sent at \"[EMAIL_RECEIVE_DATE]\" with the subject \"[EMAIL_SUBJECT]\"",
+  "text_body": "Here is a 240 character sample of the email body: \"[EMAIL_BODY]\"\n\nThe email was categorized as \"[CLASSIFICATION]\" with a [CLASSIFICATION_CONFIDENCE_SCORE]% confidence.",
   "html_body": "<html><head></head><body><p>Here is a 240 character sample of the email body:</p><p>[EMAIL_BODY]</p><p>The email was categorized as [CLASSIFICATION] with a [CLASSIFICATION_CONFIDENCE_SCORE]% confidence.</p></body></html>"
 }
 
@@ -29,52 +34,116 @@ REPLY_TEMPLATE = {
 def lambda_handler(event, context):
 
     # Print formatted JSON event object to log
+    t1 = time.time()
+    print("##### START_TIME = {} #####".format(t1-t1))
     logger.info('event={}'.format(json.dumps(event, indent=2, sort_keys=False)))
+    mybucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    # mybucket = 'deepak-email-bucket'
+    # key = "llgd185fdqlg4qd5sls6rmgo5j6oph13ho981fg1"
+    print(mybucket, key)
+    
+    s3 = boto3.client('s3')
+    raw_email = s3.get_object(
+        Bucket=mybucket,
+        Key=key)
+    raw_email_body = raw_email.get('Body').read().decode()
+    origin_message = email.message_from_string(raw_email_body)
+    email_regex =  r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    
+    # if re.search(email_regex, origin_message['From']) is not None:
+    #     email_indices = re.search(email_regex, origin_message['Return-Path']).span()
+    #     origin_sender = origin_message['Return-Path'][email_indices[0]:email_indices[-1]]
+    # else:
+    #     raise Exception("{} is not a valid email address!!!".format(message['Return-Path']))
+    origin_sender = origin_message['From']
+    
+    print(origin_sender)
+    receive_date = origin_message['Date']
+    origin_subject = origin_message['Subject']
+    body_list = [p.get_payload() for p in origin_message.get_payload()]
+    origin_body = body_list[0]
+    logger.info(body_list)
+    print(origin_body)
+    logger.info(origin_body)
+    
+    sagemaker_client = boto3.client('sagemaker')
+    ep_list = sagemaker_client.list_endpoints()['Endpoints']
+    
+    for ep in ep_list:
+        if 'sms-spam-classifier' in ep['EndpointName'] and ep['EndpointStatus']=='InService':
+            endpoint_name = ep['EndpointName']
+        else:
+            raise Exception("No valid Sagemaker endpoint available.")
+    print("EndpointName = ", endpoint_name)
+    sagemaker_runtime = boto3.client('sagemaker-runtime')
+    
+    def np2csv(arr):
+        csv = io.BytesIO()
+        np.savetxt(csv, arr, delimiter=',', fmt='%g')
+        return csv.getvalue().decode().rstrip()
 
-    # Get the Prediction Engine endpoint environment variable
-    ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
-    logger.info('Prediction Engine Endpoint={}'.format(ENDPOINT_NAME))
-
-    # ################################## #
-    # TODO: DO YOUR SAGEMAKER WORK HERE! #
-    # ################################## #
-
-    # These values are used in email reply string substitution below
-    orig_sender = 'Steve Viens<steve@viens.net>'
-    receive_date = 'ORIG-DATE'
-    orig_subject = 'ORIG-SUBJECT'
-    orig_body = 'ORIG-BODY'
-    msg_class = 'MSG-CLASS'
-    msg_score = 'MSG-SCORE'
-
+    origin_body_no_escape = origin_body.replace('\n',' ').replace('\r',' ')
+    
+    print(origin_body_no_escape)
+    logger.info(origin_body_no_escape)
+    origin_body_no_escape_list = [origin_body_no_escape]
+    one_hot_origin_body = one_hot_encode(origin_body_no_escape_list, vocabulary_length)
+    vectorized_origin_body = vectorize_sequences(one_hot_origin_body, vocabulary_length)
+    print("One hot ", one_hot_origin_body)
+    print("vectorized_origin_body shape = ", vectorized_origin_body.shape)
+    print("----------")
+    origin_body_bytestream = np2csv(vectorized_origin_body)
+    a = len(origin_body_bytestream)
+    b = type(origin_body_bytestream)
+    print("origin_body_bytestream length = {}, type = {}".format(a,b)) # .format(len(origin_body_bytestream),type(origin_body_bytestream)))
+    
+    response = sagemaker_runtime.invoke_endpoint(EndpointName='sms-spam-classifier-ll-2021-12-14-07-17-07-278', 
+                                  ContentType='text/csv',
+                                  Body=origin_body_bytestream)
+    result = json.loads(response['Body'].read().decode())
+    prediction_score = result['predictions'][0]['score']
+    spamOrHam = result['predictions'][0]['predicted_label']
+    print("Score = {}, SpamOrHam={}".format(prediction_score, spamOrHam))
+    
+    msg_score = str(prediction_score)
+    if spamOrHam==1:
+        msg_class = 'SPAM'
+    else:
+        msg_class = 'NOT SPAM'
+    
     # Reply email message string substitution
     reply = REPLY_TEMPLATE
 
     subject = reply.get('subject')
     subject = subject.replace('[EMAIL_RECEIVE_DATE]', receive_date)
-    subject = subject.replace('[EMAIL_SUBJECT]', orig_subject)
+    subject = subject.replace('[EMAIL_SUBJECT]', origin_subject)
 
     htmlBody = reply.get('html_body')
-    htmlBody = htmlBody.replace('[EMAIL_BODY]', orig_body)
+    htmlBody = htmlBody.replace('[EMAIL_BODY]', origin_body)
     htmlBody = htmlBody.replace('[CLASSIFICATION]', msg_class)
     htmlBody = htmlBody.replace('[CLASSIFICATION_CONFIDENCE_SCORE]', msg_score)
 
     textBody = reply.get('text_body')
-    textBody = textBody.replace('[EMAIL_BODY]', orig_body)
+    textBody = textBody.replace('[EMAIL_BODY]', origin_body)
     textBody = textBody.replace('[CLASSIFICATION]', msg_class)
     textBody = textBody.replace('[CLASSIFICATION_CONFIDENCE_SCORE]', msg_score)
 
     # Construct reply email
-    reply['to_recipients'] = orig_sender
+    reply['to_recipients'] = origin_sender
     reply['subject'] = subject
     reply['html_body'] = htmlBody
     reply['text_body'] = textBody
-
+    print("---------------------")
+    print("---------------------")
+    print(reply)
+    print("---------------------")
+    print("---------------------")
     # Send report via email (SES)
     try:
 
         logger.info('Reply email to send via SES: {}'.format(json.dumps(reply, indent=2, sort_keys=False)))
-        reply_to_sender(reply)
+        reply_to_sender(reply, t1)
 
     except ClientError as e:
 
@@ -92,8 +161,7 @@ def lambda_handler(event, context):
             'body': json.dumps('Reply email sent!')
         }
 
-
-def reply_to_sender(email_msg):
+def reply_to_sender(email_msg, t1):
 
     # Extract email properties
     from_sender = email_msg.get('from_sender', '')
@@ -133,10 +201,21 @@ def reply_to_sender(email_msg):
 
     # Create an SES client & send email message
     client = boto3.client('ses')
+    t2 = time.time()-t1
+    print("##### before sen TIME = {} #####".format(t2))
+    print('-------------------')
+    print('--------FINAL MESSAGE-----------')
+    final_message = msg.as_string()
+    print(final_message)
+    print('-------------------')
+    print('-------------------')
     response = client.send_raw_email(
         RawMessage={
-            'Data': msg.as_string(),
+            'Data': final_message,
         }
     )
-
+    print("FINAL TIME = {}".format(time.time()-t1))
+    logger.info("RESPONSE:\n{}".format(response))
     return response
+
+    
